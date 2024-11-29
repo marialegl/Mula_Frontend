f3d6e83370a0570bb07cda77e3b0a88349e8b371
marialegl <psico.marialegl@gmail.com>
2024-11-28 23:41:55 -0500

    fix errors

--- composeApp/src/androidMain/java/com/mula/lamulaappc/client/Bottom_Nav_Fragments_Client/FragmentMyOrdersC.kt
+++ composeApp/src/androidMain/java/com/mula/lamulaappc/client/Bottom_Nav_Fragments_Client/FragmentMyOrdersC.kt
@@ -5,23 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mula.lamulaappc.R
 
 class FragmentMyOrdersC : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_my, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = ProductAdapter(getProducts())
     }
 
     private fun getProducts(): List<Product> {
-        // Aquí se cargarían los datos de los productos desde una fuente de datos
         return listOf(
-            Product("Brasil Mogiana - Nature", "23€/KG a partir de 1000g", listOf("nature", "brasil", "espresso", "chocolat")),
+            Product("Brasil Mogiana - Nature", "23€/KG a partir de 1000g", listOf("nature", "brasil", "espresso", "chocolat"))
             // ... otros productos
         )
     }
 }
+
+data class Product(
+    val name: String,
+    val price: String,
+    val tags: List<String>
+)
